cdb63067d11738d0e1e893babf7daa7994468cab
Szyrex <sachinarunachalam54@gmail.com>
2026-02-23 14:17:25 +0530

    Coded UC1 to Print OOPS to Console

---
+++ UC1_OOPS.java
@@ -0,0 +1,13 @@
+/**
+*OOPS-banner-app UC1 - OOPS-banner-app DISPLAY APPLICATION
+* This class demostrates a simple java application that displys the object 
+* Oriented programming system OOPS acronym to the conso;e.
+*
+*@author Developer
+*@versiom 1.0
+*/
+public class UC1_OOPS-banner-app{
+	public static void main(String[] args){
+		System.out.println(x:"OOPS");
+	}
+}	
